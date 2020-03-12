a2f11d680bfd8664fbf8a25a5b7161aa64871058
Ib Green <ib@unfolded.ai>
2020-03-11 19:00:32 -0700

    prop-types: can now iterate over and compare layer props (#7)

--- cpp/tests/core/lib/layer.spec.cpp
+++ cpp/tests/core/lib/layer.spec.cpp
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:cpp/src/core/shaderlib/misc/geometry.js
-// Copyright (c) 2015 - 2017 Uber Technologies, Inc.
-=======
 // Copyright (c) 2020, Unfolded Inc
->>>>>>> prop-types: can now iterate over and compare layer props:cpp/tests/core/lib/layer.spec.cpp
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,33 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-<<<<<<< HEAD:cpp/src/core/shaderlib/misc/geometry.js
-const vs = `
-struct VertexGeometry {
-  vec4 position;
-  vec3 worldPosition;
-  vec3 worldPositionAlt;
-  vec3 normal;
-  vec2 uv;
-  vec3 pickingColor;
-} geometry;
-`;
-
-const fs = `
-#define SMOOTH_EDGE_RADIUS 0.5
-
-struct FragmentGeometry {
-  vec2 uv;
-} geometry;
-
-float smoothedge(float edge, float x) {
-  return smoothstep(edge - SMOOTH_EDGE_RADIUS, edge + SMOOTH_EDGE_RADIUS, x);
-}
-`;
-
-export default {name: 'geometry', vs, fs};
-=======
 #include <gtest/gtest.h>
 
 #include "../../../src/core/lib/layer.h"
@@ -65,4 +34,3 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
->>>>>>> prop-types: can now iterate over and compare layer props:cpp/tests/core/lib/layer.spec.cpp
