011221ed90a920aeff960127e97a729926e40973
Isaac Brodsky <isaac@isaacbrodsky.com>
2020-03-13 16:11:17 -0700

    file header

--- cpp/src/math.gl/web-mercator/web-mercator-utils.h
+++ cpp/src/math.gl/web-mercator/web-mercator-utils.h
@@ -1,3 +1,23 @@
+// Copyright (c) 2020, Unfolded Inc
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #ifndef MATHGL_WEB_MERCATOR_H
 #define MATHGL_WEB_MERCATOR_H
 
@@ -23,25 +43,25 @@ struct DistanceScales {
 };
 
 struct ViewMatrixOptions {
-  mathgl::Matrix4d viewMatrix;
+  Matrix4d viewMatrix;
   // Anchor: lng lat zoom makes viewport work w/ geospatial coordinate systems
   double longitude;
   double latitude;
   double zoom;
   // Anchor position offset (in meters for geospatial viewports)
-  mathgl::Vector3d position;
+  Vector3d position;
   // A model matrix to be applied to position, to match the layer props API
-  mathgl::Matrix4d modelMatrix;
+  Matrix4d modelMatrix;
   // Only needed for orthographic views
   double focalDistance;
-  mathgl::DistanceScales distanceScales;
+  DistanceScales distanceScales;
 
   ViewMatrixOptions();
 };
 
 struct ProjectionMatrixOptions {
   // Projection matrix
-  mathgl::Matrix4d projectionMatrix;
+  Matrix4d projectionMatrix;
 
   // Projection matrix parameters, used if projectionMatrix not supplied
   bool orthographic;
