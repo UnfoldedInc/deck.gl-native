4f16da7de29c759e91e6d0e022e5f7d00e3feffe
Ib Green <ib@unfolded.ai>
2020-03-17 11:01:14 -0700

    build: Add build-gcc.sh script

---
+++ scripts/build-gcc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p build/gcc
+cd build/gcc
+cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_C_COMPILER=gcc-9 -DCMAKE_CXX_COMPILER=gcc-9 ../..
+make
+ctest --output-on-failure
+# make test #  same output as `ctest`?
--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
-make
-ctest --output-on-failure
-# make test #  same output as `ctest`?
