4fcab7ad9722187c42df1378f54507f94a930039
Ib Green <ib@unfolded.ai>
2020-03-18 13:26:55 -0700

    modules: group src and test by module (#40)

--- cpp/src/deck.gl/json.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECKGL_JSON_H
-#define DECKGL_JSON_H
-
-#include "./json/component/component.h"  // {Component, Component::Props, PropertyTypes..}
-#include "./json/converter/json-converter.h"
-#include "./json/converter/json-types-mathgl.h"
-#include "./json/converter/json-types.h"  // {fromJson}
-
-#endif  // DECKGL_JSON_H
