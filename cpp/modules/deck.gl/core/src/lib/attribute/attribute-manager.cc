66a21579a4454460a27db35094f3d720be294c51
Ib Green <ib@unfolded.ai>
2020-03-22 14:47:57 -0700

    deck.gl/core: LayerManager improvements

--- cpp/modules/deck.gl/core/src/lib/attribute/attribute-manager.cc
+++ cpp/modules/deck.gl/core/src/lib/attribute/attribute-manager.cc
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include "attribute-manager.h"  // NOLINT(build/include)
+#include "./attribute-manager.h"  // NOLINT(build/include)
 
 #include <iostream>
 
