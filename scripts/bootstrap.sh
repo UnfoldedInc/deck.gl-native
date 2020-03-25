0c41efb3dc498b876d6aa60b5e8499b5b3c3a703
Isaac Brodsky <isaac@isaacbrodsky.com>
2020-03-25 16:15:22 -0700

    don't use vcpkg in CI

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
 VCPKG_ROOT=${VCPKG_ROOT:-../vcpkg}
-"${VCPKG_ROOT}/vcpkg" install jsoncpp
-"${VCPKG_ROOT}/vcpkg" install gtest
-"${VCPKG_ROOT}/vcpkg" install range-v3
+if [ -d "${VCPKG_ROOT}" ]; then
+  "${VCPKG_ROOT}/vcpkg" install jsoncpp
+  "${VCPKG_ROOT}/vcpkg" install gtest
+  "${VCPKG_ROOT}/vcpkg" install range-v3
+else
+  echo "vcpkg not found, skipping (you might need to set VCPKG_ROOT)"
+fi
