78f21ac41bf8233c6fa06c9c1390eb3fb9bac42d
Ib Green <ib@unfolded.ai>
2020-03-26 06:38:23 -0700

    wip

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
 VCPKG_ROOT=${VCPKG_ROOT:-../vcpkg}
+
+## General C++ Dependencies
 "${VCPKG_ROOT}/vcpkg" install jsoncpp
 "${VCPKG_ROOT}/vcpkg" install gtest
 "${VCPKG_ROOT}/vcpkg" install range-v3
-"${VCPKG_ROOT}/vcpkg" install glfw3
+"${VCPKG_ROOT}/vcpkg" install fmt
