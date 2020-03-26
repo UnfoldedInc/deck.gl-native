815c4a1b22111dd7a58c9086b697a35df37d4129
Ib Green <ib@unfolded.ai>
2020-03-26 06:35:35 -0700

    wip

--- cpp/modules/probe.gl/core/src/timer.cpp
+++ cpp/modules/probe.gl/core/src/timer.cpp
@@ -19,22 +19,22 @@ using namespace probegl;
 #if defined(PROBEGL_PLATFORM_APPLE)
 
 void Timer::start() {
-  this->mStartTime = mach_absolute_time();
+  this->startTime = mach_absolute_time();
   // Cache secondCoeff
   this->_getSecondCoeff();
-  this->mRunning = true;
+  this->running = true;
 }
 
 void Timer::stop() {
-  this->mStopTime = mach_absolute_time();
-  this->mRunning = false;
+  this->stopTime = mach_absolute_time();
+  this->running = false;
 }
 
 auto Timer::getElapsedTime() const -> double {
-  if (this->mRunning) {
-    return this->mSecondCoeff * (mach_absolute_time() - this->mStartTime);
+  if (this->running) {
+    return this->secondCoeff * (mach_absolute_time() - this->startTime);
   } else {
-    return this->mSecondCoeff * (this->mStopTime - this->mStartTime);
+    return this->secondCoeff * (this->stopTime - this->startTime);
   }
 }
 
@@ -42,96 +42,96 @@ auto Timer::getAbsoluteTime() -> double { return this->_getSecondCoeff() * mach_
 
 auto Timer::_getSecondCoeff() -> double {
   // If this is the first time we've run, get the timebase.
-  if (this->mSecondCoeff == 0.0) {
+  if (this->secondCoeff == 0.0) {
     mach_timebase_info_data_t timebaseInfo;
     mach_timebase_info(&timebaseInfo);
-    this->mSecondCoeff = timebaseInfo.numer * (1.0 / 1000000000) / timebaseInfo.denom;
+    this->secondCoeff = timebaseInfo.numer * (1.0 / 1000000000) / timebaseInfo.denom;
   }
 
-  return this->mSecondCoeff;
+  return this->secondCoeff;
 }
 
 #elif defined(PROBEGL_PLATFORM_POSIX)
 
-static uint64_t GetCurrentTimeNs() {
+static uint64_t getCurrentTimeNs() {
   struct timespec currentTime;
   clock_gettime(CLOCK_MONOTONIC, &currentTime);
   return currentTime.tv_sec * 1'000'000'000llu + currentTime.tv_nsec;
 }
 
-void Timer::Start() {
-  mStartTimeNs = GetCurrentTimeNs();
-  mRunning = true;
+void Timer::start() {
+  this->startTimeNs = getCurrentTimeNs();
+  this->running = true;
 }
 
-void Timer::Stop() {
-  mStopTimeNs = GetCurrentTimeNs();
-  mRunning = false;
+void Timer::stop() {
+  this->stopTimeNs = getCurrentTimeNs();
+  this->running = false;
 }
 
-double Timer::GetElapsedTime() const {
+double Timer::getElapsedTime() const {
   uint64_t endTimeNs;
-  if (mRunning) {
-    endTimeNs = GetCurrentTimeNs();
+  if (this->running) {
+    endTimeNs = getCurrentTimeNs();
   } else {
-    endTimeNs = mStopTimeNs;
+    endTimeNs = this->stopTimeNs;
   }
 
-  return (endTimeNs - mStartTimeNs) * 1e-9;
+  return (endTimeNs - this->startTimeNs) * 1e-9;
 }
 
-double Timer::GetAbsoluteTime() { return GetCurrentTimeNs() * 1e-9; }
+double Timer::getAbsoluteTime() { return getCurrentTimeNs() * 1e-9; }
 
 #elif defined(PROBEGL_PLATFORM_WINDOWS)
 
-void Timer::Start() {
+void Timer::start() {
   LARGE_INTEGER curTime;
   QueryPerformanceCounter(&curTime);
-  mStartTime = curTime.QuadPart;
+  this->startTime = curTime.QuadPart;
 
   // Cache the frequency
-  GetFrequency();
+  this->getFrequency();
 
-  mRunning = true;
+  this->running = true;
 }
 
-void Timer::Stop() {
+void Timer::stop() {
   LARGE_INTEGER curTime;
   QueryPerformanceCounter(&curTime);
-  mStopTime = curTime.QuadPart;
+  this->stopTime = curTime.QuadPart;
 
-  mRunning = false;
+  this->running = false;
 }
 
-double Timer::GetElapsedTime() const {
+double Timer::getElapsedTime() const {
   LONGLONG endTime;
-  if (mRunning) {
+  if (running) {
     LARGE_INTEGER curTime;
     QueryPerformanceCounter(&curTime);
     endTime = curTime.QuadPart;
   } else {
-    endTime = mStopTime;
+    endTime = this->stopTime;
   }
 
-  return static_cast<double>(endTime - mStartTime) / mFrequency;
+  return static_cast<double>(endTime - this->startTime) / this->frequency;
 }
 
-double Timer::GetAbsoluteTime() {
+double Timer::getAbsoluteTime() {
   LARGE_INTEGER curTime;
   QueryPerformanceCounter(&curTime);
 
-  return static_cast<double>(curTime.QuadPart) / GetFrequency();
+  return static_cast<double>(curTime.QuadPart) / this->getFrequency();
 }
 
-LONGLONG Timer::GetFrequency() {
-  if (mFrequency == 0) {
+LONGLONG Timer::getFrequency() {
+  if (this->frequency == 0) {
     LARGE_INTEGER frequency = {};
     QueryPerformanceFrequency(&frequency);
 
-    mFrequency = frequency.QuadPart;
+    this->frequency = frequency.QuadPart;
   }
 
-  return mFrequency;
+  return this->frequency;
 }
 
 #endif
