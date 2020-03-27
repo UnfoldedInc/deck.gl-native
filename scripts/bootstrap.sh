9b3c2e7c8cfe4d43ff1b8e1742c92ac2d5cafa86
Ib Green <ib@unfolded.ai>
2020-03-27 13:09:22 -0700

    Build docs: Successfully build dawn examples

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,16 @@
 #/bin/bash
-
 VCPKG_ROOT=${VCPKG_ROOT:-../vcpkg}
 
-## General C++ Dependencies
-"${VCPKG_ROOT}/vcpkg" install jsoncpp
-"${VCPKG_ROOT}/vcpkg" install gtest
-"${VCPKG_ROOT}/vcpkg" install range-v3
-"${VCPKG_ROOT}/vcpkg" install fmt
+## Platform independent bootstrap (mainly vcpkg installs)
+./scripts/internal/bootstrap-common.sh
+
+## Graphics dependencies for luma.gl
+"${VCPKG_ROOT}/vcpkg" install glfw3
+"${VCPKG_ROOT}/vcpkg" install shaderc # TODO(ib@unfolded.ai): compiling shaderc from vcpkg is very slow!
+
+## Install loaders.gl dependencies
+brew install gdal
+
+# Install build tools
+brew update
+brew install gcc cmake clang-format lcov
--- scripts/build-cpplint.sh
+++ scripts/build-cpplint.sh
@@ -7,6 +7,6 @@ VCPKG_ROOT=${VCPKG_ROOT:-../vcpkg}
 mkdir -p build/cpplint
 cd build/cpplint
 cmake "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" "-DCMAKE_CXX_CPPLINT=cpplint" ../..
-make -j
+make -j 16
 ctest --output-on-failure
 # make test #  same output as `ctest`?
