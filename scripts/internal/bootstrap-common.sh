8a272f98baa27ae4c2dd2fb55401262f9e868595
Isaac Brodsky <isaac@isaacbrodsky.com>
2020-04-01 12:22:30 -0700

    Clone dependencies repo in CI

--- scripts/internal/bootstrap-common.sh
+++ scripts/internal/bootstrap-common.sh
@@ -3,7 +3,9 @@
 VCPKG_ROOT=${VCPKG_ROOT:-../vcpkg}
 
 ## General C++ Dependencies
-"${VCPKG_ROOT}/vcpkg" install jsoncpp
-"${VCPKG_ROOT}/vcpkg" install gtest
-"${VCPKG_ROOT}/vcpkg" install range-v3
-"${VCPKG_ROOT}/vcpkg" install fmt
+if [ "$SKIP_VCPKG" != "true" ] ; then
+  "${VCPKG_ROOT}/vcpkg" install jsoncpp
+  "${VCPKG_ROOT}/vcpkg" install gtest
+  "${VCPKG_ROOT}/vcpkg" install range-v3
+  "${VCPKG_ROOT}/vcpkg" install fmt
+fi
