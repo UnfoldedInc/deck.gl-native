7224117192517527b0249c0451e699879d7a1ef4
randypalusz <ibanezguy998@gmail.com>
2020-04-29 12:58:59 -0400

    Mostly complete header file - missing a few methods

--- cpp/modules/deck.gl/layers/src/solid-polygon-layer/solid-polygon-layer.h
+++ cpp/modules/deck.gl/layers/src/solid-polygon-layer/solid-polygon-layer.h
@@ -30,18 +30,55 @@ namespace deckgl {
 
 class SolidPolygonLayer : public Layer{
  public:
-    class Props;
-    explicit SolidPolygonLayer(std::shared_ptr<SolidPolygonLayer::Props> props)
+  class Props;
+  explicit SolidPolygonLayer(std::shared_ptr<SolidPolygonLayer::Props> props)
       : Layer{std::dynamic_pointer_cast<Layer::Props>(props)} {}
-    auto props() { return std::dynamic_pointer_cast<Layer::Props>(this->_props); }
+  auto props() { return std::dynamic_pointer_cast<Layer::Props>(this->_props); }
+
+  auto getPolygon(const std::shared_ptr<arrow::Table>& table) -> std::shared_ptr<arrow::Array>;
+  auto getElevationData(const std::shared_ptr<arrow::Table>& table) -> std::shared_ptr<arrow::Array>;
+  auto getFillColorData(const std::shared_ptr<arrow::Table>& table) -> std::shared_ptr<arrow::Array>;
+  auto getLineColorData(const std::shared_ptr<arrow::Table>& table) -> std::shared_ptr<arrow::Array>;
  protected:
+  void initializeState() override;
+  // auto getPickingInfo() override;
+  void updateState(const ChangeFlags&, const Layer::Props* oldProps) override;
+  // void updateGeometry(const ChangeFlag &, const Layer::Props* oldProps) override;
+  void finalizeState() override;
+  void drawState(wgpu::RenderPassEncoder pass) override;
  private:
+  auto _getModel(wgpu::Device device) -> std::shared_ptr<lumagl::Model>;
 };
 
 class SolidPolygonLayer::Props : public Layer::Props {
  public:
- protected:
- private:
+  using super = Layer::Props;
+  static constexpr const char* getTypeName() { return "SolidPolygonLayer"; }
+
+  // Property Type Machinery
+  auto getProperties() const -> const Properties* override;
+  auto makeComponent(std::shared_ptr<Component::Props> props) const -> SolidPolygonLayer* override {
+    return new SolidPolygonLayer{std::dynamic_pointer_cast<SolidPolygonLayer::Props>(props)};
+  }
+
+  bool filled{true};
+  bool extruded{false};
+  bool wireframe{false};
+  bool material{true};
+
+  // _normalize in JS, intended to be private?
+  bool normalize{true};
+
+  float elevationScale{1.0};
+
+  std::function<ArrowMapper::Vector4FloatAccessor> getPolygon{
+    [](const Row& row) { return row.getFloatVector4("polygon"); }};
+  std::function<ArrowMapper::FloatAccessor> getElevation{
+    [](const Row& row) { return 1000.0; }};
+  std::function<ArrowMapper::Vector4FloatAccessor> getFillColor{
+    [](const Row&) { return mathgl::Vector4<float>(0.0, 0.0, 0.0, 255.0); }};
+  std::function<ArrowMapper::Vector4FloatAccessor> getLineColor{
+    [](const Row&) { return mathgl::Vector4<float>(0.0, 0.0, 0.0, 255.0); }};
 };
 
 }  // namespace deckgl
