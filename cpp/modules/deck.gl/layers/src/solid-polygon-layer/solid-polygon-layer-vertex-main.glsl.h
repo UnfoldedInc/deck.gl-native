b2e704a053cbf939911ffae6a830f704b454e014
randypalusz <ibanezguy998@gmail.com>
2020-05-11 14:27:03 -0400

    Added vertex-side shader

--- cpp/modules/deck.gl/layers/src/solid-polygon-layer/solid-polygon-layer-vertex-main.glsl.h
+++ cpp/modules/deck.gl/layers/src/solid-polygon-layer/solid-polygon-layer-vertex-main.glsl.h
@@ -110,6 +110,7 @@ void calculatePosition(PolygonProps props){
 
 }  // anonymous namespace
 
+// not sure that the following "vsm" definition is necessary - vs defined in side layer
 // NOLINTNEXTLINE(runtime/string)
 static const std::string vsm = "#version450\n" + geometryVS + "\n" + project32VS + "\n" + solidPolygonLayerVSM;
 
