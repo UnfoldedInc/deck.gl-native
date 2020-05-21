ca7dcc206ea811b76071211242c8a10000ee69ec
Ilija Puaca <ilija@unfolded.ai>
2020-05-21 22:27:48 +0800

    Build scripts revisited, build config tuneup (#142)

--- scripts/build-clang.sh
+++ scripts/build-clang.sh
@@ -2,9 +2,6 @@
 
 # Note when switching compiler: `rm -fr build`
 
-mkdir -p build/clang
-cd build/clang
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ../..
-make -j 16
-ctest --output-on-failure
-# make test #  same output as `ctest`?
+cmake -S . -B build/clang -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+cmake --build build/clang -j 16
+build/clang/deckgl-bundle-tests
